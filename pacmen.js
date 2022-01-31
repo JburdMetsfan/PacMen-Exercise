44555eae512c80e7315b90636e6669f40e4d857f
Jeanne Burdick <jeanne_burdick@yahoo.com>
2022-01-31 16:34:49 -0500

    updates to movement

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ['Images/PacMenOG.png', 'Images/PacMan2.png'],
+  ['Images/PacMan3.png', 'Images/PacMan4.png'],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -24,9 +24,7 @@ function makePac() {
   let game = document.getElementById('game');
   let newimg = document.createElement('img');
   newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
-  newimg.open = 0;
-  newimg.count = 0;
+  newimg.src = 'Images/PacManOG.png';
   newimg.width = 100;
 
   // TODO: set position here
